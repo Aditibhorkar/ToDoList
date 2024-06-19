47c41236b6dfb89d6a21beb51d838e3ad2773304
Aditibhorkar <127037400+Aditibhorkar@users.noreply.github.com>
2024-06-19 22:35:26 +0530

    Add files via upload

---
+++ Task.java
@@ -0,0 +1,29 @@
+package ToDoList;
+
+public class Task {
+
+	private String description;
+	private boolean isCompleted;
+
+	public Task(String description) {
+		this.description = description;
+		this.isCompleted = false;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public boolean isCompleted() {
+		return isCompleted;
+	}
+
+	public void markAsCompleted() {
+		this.isCompleted = true;
+	}
+
+	@Override
+	public String toString() {
+		return (isCompleted ? "[Y] " : "[X] ") + description;
+	}
+}
